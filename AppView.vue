345a2c964d382b1f6e1025b6b1d33e14984f50bb
goohayami <goohayami@gmail.com>
2022-12-16 08:32:48 +0900

    he

---
+++ AppView.vue
@@ -0,0 +1,17 @@
+<template>
+  <p>Vue3 CDNでVue-routerを使う！</p>
+  <h1>Hello App!</h1>
+  <nav>
+    <router-link to="/">Go to Home</router-link> |
+    <router-link to="/about">Go to About</router-link>
+  </nav>
+
+  <router-view></router-view>
+</template>
+
+<style>
+nav{
+  background-color: bisque;
+  
+}
+</style>
---
+++ views/AboutView.vue
@@ -0,0 +1,3 @@
+<template>
+  <p>ここはアバウト</p>
+</template>
